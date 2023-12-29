946ca8e90c5363dd6b93a9d1f4766ad9376b17ed
Aditya singh chauhan <adityaschauhan346@gmail.com>
2023-12-29 19:20:31 +0530

    Create template_1.cpp

---
+++ template_1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <cmath>
+#include <iomanip>
+#include <climits>
+#include <bitset>
+
+typedef unsigned long long ull;
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        
+    }
+    return 0;
+}
