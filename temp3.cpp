ceddd32ece2b9bd2c50b7c74e696ea39671cd665
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-01-21 01:30:53 +0530

    Update temp3.cpp

--- temp3.cpp
+++ temp3.cpp
@@ -11,13 +11,15 @@
 
 
 #define pb push_back
-#define mod 1000000007
+#define MOD (int)(1e9+7)
 #define vll vector<ll>
 #define all(v) v.begin(),v.end()
 #define sz(v) (int)(v.size())
 #define srt(v)  sort(v.begin(),v.end())         // sort 
 #define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
 #define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b)))
 
 typedef unsigned long long ull;
 typedef long long ll;
@@ -26,7 +28,7 @@ using namespace std;
 
 // ====================================================================
 
-int main() {
+int32_t main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
