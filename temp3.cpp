85bf0d2e20af5b9f1d0a7249dc87f42e605b03f1
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-01-21 00:49:28 +0530

    Create temp3.cpp

---
+++ temp3.cpp
@@ -0,0 +1,41 @@
+/*
+  कर्म से ही विजय है भाग्य भी कर्म पर निर्भर है कर्म है तो सफलता तय है।
+*/
+
+// ====================================================================
+
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+
+#define pb push_back
+#define mod 1000000007
+#define vll vector<ll>
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+
+typedef unsigned long long ull;
+typedef long long ll;
+
+using namespace std;
+
+// ====================================================================
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+       
+    }
+    return 0;
+}
+
